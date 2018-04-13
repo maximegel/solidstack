9410618f75b2176a626013425f010b119211480e
Maxime Gélinas <mgelinas@softframe.net>
2018-04-12 21:18:50 -0400

    Feature/1 (#1)
    * Implement and test `Guard` class
    
    * Fix `.travis.yml`
    
    * Fix `.travis.yml` and update `README.md`
    
    * Update `README.md`
    
    * Remove `Debug.Assert` usage and cover all tests

---
+++ src/SolidStack.Testing.Xunit/DebugOnlyFactAttribute.cs
@@ -0,0 +1,11 @@
+﻿using SolidStack.Testing.Xunit.Internal;
+using Xunit;
+
+namespace SolidStack.Testing.Xunit
+{
+    public class DebugOnlyFactAttribute : FactAttribute
+    {
+        public DebugOnlyFactAttribute() =>
+            FactSkipper.SkipInRelease(this);
+    }
+}
---
+++ src/SolidStack.Testing.Xunit/DebugOnlyTheoryAttribute.cs
@@ -0,0 +1,11 @@
+﻿using SolidStack.Testing.Xunit.Internal;
+using Xunit;
+
+namespace SolidStack.Testing.Xunit
+{
+    public class DebugOnlyTheoryAttribute : TheoryAttribute
+    {
+        public DebugOnlyTheoryAttribute() =>
+            FactSkipper.SkipInRelease(this);
+    }
+}
